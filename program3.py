057d0e33ae6d3e6f55a6ab113c076527c5b19586
JhalakGupta16 <44206449+JhalakGupta16@users.noreply.github.com>
2020-05-19 20:15:33 +0530

    Create program3.py

---
+++ program3.py
@@ -0,0 +1,8 @@
+a = int(input("Enter the value of a: ")) # 7
+b = int(input("Enter the value of b: ")) # 8
+a = a + b # a = 15, b = 8
+b = a - b # b = 7
+a = a - b # a = 8
+print("After Swaping")
+print("Value of a =", a)
+print("Value of b =", b)
